c3f330e55e297c00befd00b93dab131e8b4132f3
yujin113 <claire9585@sookmyung.ac.kr>
2022-06-26 02:03:47 +0900

    model 수정

--- myblog/blogapp/models.py
+++ myblog/blogapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    date = models.DateTimeField(auto_now_add=True)
     body = models.TextField()
 
     def __str__(self):
